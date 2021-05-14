3520a2029b75ecdb4fe9c301c1c20e561c0695f6
Subham Deb <subham1997deb@gmail.com>
2021-05-14 19:21:35 +0530

    onair

--- face-detector.py
+++ face-detector.py
@@ -1,2 +1,18 @@
 import cv2;
 print("Completed");
+
+trained_face_data = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+img = cv2.imread("rdj.jpg")
+
+
+grayscaled_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+face_coordinates = trained_face_data.detectMultiScale(grayscaled_img)
+
+cv2.imshow("Hello, I'm Subham Deb",img)
+cv2.waitKey()
+
+# cv2.imshow("Hello, I'm Subham Deb",grayscaled_img)
+# cv2.waitKey()
